8dfc08a21b90498b03208bc459d22826a6d26605
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2019-03-17 15:35:55 +0100

    Adds source command

--- src/index.js
+++ src/index.js
@@ -5,5 +5,12 @@ const bot = new telegraf(process.env.BOT_TOKEN);
 const dayOfYear = getDayOfYear(new Date());
 const percentage = (dayOfYear / 365) * 100;
 
-bot.start(ctx => ctx.reply(`${percentage.toFixed(2)}%`));
+function getProgress() {
+  return `${percentage.toFixed(2)}%`;
+}
+
+bot.start(ctx => ctx.reply(getProgress()));
+bot.command("source", ctx =>
+  ctx.reply("https://github.com/malcodeman/year-progress-bot.git")
+);
 bot.launch();
